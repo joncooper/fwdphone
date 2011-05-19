05dcbc8218ee74a2382a66206a2c5e120c9be26d
Jon Cooper <jon.cooper@gmail.com>
2011-05-19 13:51:02 -0700

    testing long SMS

--- fwdphone.rb
+++ fwdphone.rb
@@ -12,6 +12,11 @@ class FwdPhone < Sinatra::Base
     end
   end
   
+  get '/longSMS' do
+    headers 'Content-Type' => 'text/xml'
+    "<Response><Sms>Crackalackin ipsum dolizzle tellivizzle amizzle, shit adipiscing elit. Nullizzle sapizzle velizzle, volutpizzle, suscipizzle quizzle, i saw beyonces tizzles and my pizzle went crizzle vizzle, arcu. Pellentesque go to hizzle nizzle. Sizzle erizzle. Cool izzle dolizzle dapibus that's the shizzle tempizzle funky fresh. Mauris brizzle nibh izzle turpizzle. Vestibulum izzle tortor. Rizzle gizzle rhoncus nisi. In hizzle habitasse shizzle my nizzle crocodizzle dictumst. Fo shizzle dapibizzle. Gangsta tellus urna, pretium gizzle, mattizzle ac, eleifend vitae, fo. Away suscipizzle. Integizzle bow wow wow velit fo shizzle purus.</Sms></Response>"
+  end
+  
   get '/incomingSMS' do
     headers 'Content-Type' => 'text/xml'
     TwiML.sms_response(params)
